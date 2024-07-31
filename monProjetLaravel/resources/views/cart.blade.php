5c4a46a0412f43ec5c6d34796c8529a467f5fc0b
AlexCampusNum <alex.chamoux@le-campus-numerique.fr>
2024-07-31 12:16:41 +0200

    finalisation details et création cart

--- monProjetLaravel/resources/views/cart.blade.php
+++ monProjetLaravel/resources/views/cart.blade.php
@@ -1,3 +1,34 @@
-<?php
+@include('header')
 
-echo 'Panier';
+<div class="container">
+    <div class="border-cart">
+        <table>
+            <tr>
+                <th>Produit</th>
+                <th></th>
+                <th>Prix unitaire</th>
+                <th>Quantité</th>
+                <th>Total</th>
+                <th></th>
+            </tr>
+            <tr>
+                <td>Nom du produit</td>
+                <td><img src="{{ asset('images/dcshoes.jpg') }}" style="width: 10vw" alt="photo du produit"></td>
+                <td>NN €</td>
+                <td>
+                    <input type="number" id="quantity-thumbnail" name="quantity" min="0">
+                </td>
+                <td>
+                    NNN €
+                </td>
+                <td>
+
+                </td>
+            </tr>
+        </table>
+
+    </div>
+
+</div>
+
+@include('footer')
--- monProjetLaravel/resources/views/thumbnail.blade.php
+++ monProjetLaravel/resources/views/thumbnail.blade.php
@@ -3,17 +3,17 @@
 
     <input name="idProduct" type="hidden" value="" />
     <div class="row">
-        <img src="" class="product-image mb-3" alt="Photo du produit">
+        <img src="{{ asset('images/dcshoes.jpg') }}" id="product-image" alt="Photo du produit">
 
         <div class="row">
             <p>
-                <label for="quantity" class="mb-3">Quantité : </label>
-                <input type="number" id="quantity" class="mb-3" name="quantity">
+                <label for="quantity">Quantité : </label>
+                <input type="number" id="quantity-thumbnail" name="quantity" min="0">
             </p>
+            <p>NNN €</p>
         </div>
+        <p><input type="submit" value="Ajouter au panier"></p>
     </div>
-    <p>Prix (TTC) : N €</p>
 
-    <p><input type="submit" value="Ajouter au panier"></p>
 </form>
 
