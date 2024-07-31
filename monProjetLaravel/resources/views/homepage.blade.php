b3342b2c5a00ce08139417dcfee9ab0cb8b651a7
while1win <yaman.mouwakeh@le-campus-numerique.fr>
2024-07-31 12:12:37 +0200

    Navbar

--- monProjetLaravel/resources/views/homepage.blade.php
+++ monProjetLaravel/resources/views/homepage.blade.php
@@ -4,33 +4,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>mon site</title>
 </head>
 <body>
 <header>
     <!-- Fixed navbar -->
-    <nav class="navbar navbar-expand-md navbar-dark  bg-dark mb-5 ps-3 ">
-        <a class="navbar-brand" href="#">Mon Site</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="item.php">Accueil </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Panier</a>
+    <nav class="navbar navbar-expand-md navbar-dark d-flex justify-content-between navbar-custom  toto mb-5 ps-3 ">
+        <div class="d-flex justify-content-between">
+            <a class="navbar-brand" href="#"><img src="{{asset('images/image.png')}}" alt="mon" height="80"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
+            <ul class="navbar-nav mr-auto mt-3">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle " href="product-list.blade.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Chaussures
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Sneakers</a></li>
+                            <li><a class="dropdown-item" href="#">Chaussures de skate</a></li>
+                            <li><a class="dropdown-item" href="#">Chaussures de montagne</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="#">Panier</a>
+                    </li>
             </ul>
-
         </div>
+        <div>
+                        <form class="d-flex mb-3 ml-auto p-2" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Rechercher</button>
+                        </form>
+        </div>
+
+
+
     </nav>
+
 </header>
 <main role="main" class="container">
-
+    <img src="{{asset('images/image.png')}}" alt="mon">
 
 </main>
 <footer class="footer">
