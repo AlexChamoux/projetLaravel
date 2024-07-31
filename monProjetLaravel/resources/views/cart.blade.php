bbe7d6569da422803f12bb9f327062920d70a18f
AlexCampusNum <alex.chamoux@le-campus-numerique.fr>
2024-07-31 14:24:06 +0200

    dernier commit avant merge

--- monProjetLaravel/resources/views/cart.blade.php
+++ monProjetLaravel/resources/views/cart.blade.php
@@ -11,20 +11,9 @@
                 <th>Total</th>
                 <th></th>
             </tr>
-            <tr>
-                <td>Nom du produit</td>
-                <td><img src="{{ asset('images/dcshoes.jpg') }}" style="width: 10vw" alt="photo du produit"></td>
-                <td>NN €</td>
-                <td>
-                    <input type="number" id="quantity-thumbnail" name="quantity" min="0">
-                </td>
-                <td>
-                    NNN €
-                </td>
-                <td>
-
-                </td>
-            </tr>
+{{--            @foreach($_SESSION['cart'] as $value)--}}
+                    @include('partial-cart')
+{{--            @endforeach--}}
         </table>
 
     </div>
